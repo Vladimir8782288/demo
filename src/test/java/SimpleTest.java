3ce1428944fbc662980349fe6d0a359a9bb73f8e
Pavel Gavrilov <gavrilov.paul@yandex.ru>
2022-01-14 22:20:13 +0300

    rename Main file, add first tests

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    void test1() {
+        final int x = 4 + 6;
+        assertEquals(10, x, "invalid value");
+    }
+
+    @Test
+    void test2() {
+        final String buf = "abcd";
+        assertEquals("test", buf, "invalid value");
+    }
+
+    @Test
+    void test3() {
+
+    }
+}
+
