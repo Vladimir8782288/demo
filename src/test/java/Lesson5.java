2a980d28c6af05d7b9248a5a6073b09f2000e8ad
Vovan <vovan844@gmail.com>
2022-01-15 00:09:02 +0300

    Ошибки оформления (вывод на экран)

---
+++ src/test/java/Lesson5.java
@@ -0,0 +1,7 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println ("What Is Dead May Never Die");
+        // END
+    }
+}
