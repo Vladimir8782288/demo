f9393965452fd7dbe2c619134f975960d35e4ffe
Vovan <vovan844@gmail.com>
2022-01-14 22:49:11 +0300

    Вывод на экран

---
+++ src/test/java/Lesson1.java
@@ -0,0 +1,7 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("Hello, World!");
+        // END
+    }
+}
