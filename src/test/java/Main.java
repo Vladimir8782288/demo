d36609c5f21e56af49ce815a64a8fcea674db25c
Vovan <vovan844@gmail.com>
2022-01-14 21:55:01 +0300

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+public class Main {
+   @Test
+    void test1(){
+
+
+    }
+    @Test
+    void test2(){
+
+
+    }
+    @Test
+    void test3(){
+
+
+    }
+}
+
