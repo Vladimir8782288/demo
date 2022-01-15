668b3f50da604868b6aec8e76fe78e8cc863aac7
Vovan <vovan844@gmail.com>
2022-01-15 19:01:26 +0300

    Линтер (соответствие стандартам)

---
+++ src/test/java/Lesson12.java
@@ -0,0 +1,7 @@
+public class Lesson12 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println((5 +2 ) - (3 * 7));
+        // END
+    }
+}
