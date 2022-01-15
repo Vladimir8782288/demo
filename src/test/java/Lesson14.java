98b4f03af1462fa5c23135759e3d2be536843ab1
Vovan <vovan844@gmail.com>
2022-01-15 21:30:13 +0300

    Экранирующие последовательности

---
+++ src/test/java/Lesson14.java
@@ -0,0 +1,7 @@
+public class Lesson14 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("- Did Joffrey agree?\n- He did. He also said \"I love using \\n\".");
+        // END
+    }
+}
