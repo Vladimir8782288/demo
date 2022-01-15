25f15a569639ddfca7c69440d9bc8d857863eb17
Vovan <vovan844@gmail.com>
2022-01-15 21:08:03 +0300

    Кавычки

---
+++ src/test/java/Lesson13.java
@@ -0,0 +1,7 @@
+public class Lesson13 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println("\"Khal Drogo's favorite word is \"athjahakar\"\"");
+        // END
+    }
+}
