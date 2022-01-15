0e51f852124ca1b329bff429aefbcb3542e6eeda
Vovan <vovan844@gmail.com>
2022-01-15 18:57:13 +0300

    Числа с плавающей точкой

---
+++ src/test/java/Lesson11.java
@@ -0,0 +1,7 @@
+public class Lesson11 {
+    public static void main(String[] args) {
+        // BEGIN (write your solution here)
+        System.out.println (0.39 * 0.22);
+        // END
+    }
+}
