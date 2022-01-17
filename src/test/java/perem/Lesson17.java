9272f913a0479f9e1687892fd0b9a9e68a4d9c8f
Vovan <vovan844@gmail.com>
2022-01-18 00:13:29 +0300

    Изменение переменной

---
+++ src/test/java/perem/Lesson17.java
@@ -0,0 +1,14 @@
+package perem;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson17 {
+    @Test
+    void test() {
+        var name = "Brienna";
+        // BEGIN (write your solution here)
+        name = "anneirB";
+        // END
+        System.out.println(name);
+    }
+}
