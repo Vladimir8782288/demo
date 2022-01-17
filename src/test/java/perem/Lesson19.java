10d5f6a55668ef8fdfa5e4d349a6852c46b3a997
Vovan <vovan844@gmail.com>
2022-01-18 00:27:48 +0300

    Ошибки при работе с переменными

---
+++ src/test/java/perem/Lesson19.java
@@ -0,0 +1,18 @@
+package perem;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson19 {
+    @Test
+    void test() {
+        var family = "Targaryen";
+
+        // BEGIN (write your solution here)
+        var pet = "Dragon";
+        // END
+
+        System.out.print(family);
+        System.out.print(" and ");
+        System.out.print(pet);
+    }
+}
