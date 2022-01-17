3b3b2e71c8d6b294e8ac452ecebdfb617cfe416a
Vovan <vovan844@gmail.com>
2022-01-18 00:23:19 +0300

    Выбор имени переменной

---
+++ src/test/java/perem/Lesson18.java
@@ -0,0 +1,14 @@
+package perem;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson18 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        var firstnumber = 1.10;
+        var secondnumber = -100;
+        System.out.println(firstnumber * secondnumber);
+        // END
+    }
+}
