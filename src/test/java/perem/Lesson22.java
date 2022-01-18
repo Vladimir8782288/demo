9f0ee83562d25299fc23a04cdf63db86e7d4322f
Vovan <vovan844@gmail.com>
2022-01-19 00:52:16 +0300

    Именование переменных

---
+++ src/test/java/perem/Lesson22.java
@@ -0,0 +1,16 @@
+package perem;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson22 {
+
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        var firstNumber = 11;
+        var secondNumber = -100;
+
+        System.out.println(firstNumber * secondNumber);
+        // END
+    }
+}
