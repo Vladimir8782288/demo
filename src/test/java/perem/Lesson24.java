5219124f7bcdd74bc3a0ebeb76bd66305e8f32c2
Vovan <vovan844@gmail.com>
2022-01-19 01:20:34 +0300

    Константы

---
+++ src/test/java/perem/Lesson24.java
@@ -0,0 +1,12 @@
+package perem;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson24 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        final var DRAGONS_BORN_COUNT = 3;
+        // END
+    }
+}
