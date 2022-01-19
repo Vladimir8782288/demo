e54a8fd2b7f4f11974d7b0a5457a30a910db7571
Vovan <vovan844@gmail.com>
2022-01-20 01:01:15 +0300

    Типы данных (строки, числа)

---
+++ src/test/java/datatypes/Lesson25.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson25 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        System.out.println(3 / 2);
+        // END
+    }
+
+}
