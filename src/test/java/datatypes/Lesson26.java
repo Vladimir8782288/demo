65e354b7e7b6f769c2ab73e53d14f4df04f1b2ac
Vovan <vovan844@gmail.com>
2022-01-20 01:02:39 +0300

    Явная типизация

---
+++ src/test/java/datatypes/Lesson26.java
@@ -0,0 +1,13 @@
+package datatypes;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson26 {
+        @Test
+        void test() {
+            // BEGIN (write your solution here)
+            String str = "One more time";
+            System.out.println(str);
+            // END
+        }
+}
