96494bb4d54af43b68ef4bbc02a79ac207177f19
Vovan <vovan844@gmail.com>
2022-01-21 01:08:30 +0300

    Значение null

---
+++ src/test/java/datatypes/Lesson28.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson28 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        String greeting;
+        // END
+
+        greeting = "Hello Hexlet!";
+        System.out.println(greeting);
+    }
+}
