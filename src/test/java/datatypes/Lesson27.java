b23449edaf52c1f354790cb9b802b00c5744c04e
Vovan <vovan844@gmail.com>
2022-01-21 00:45:43 +0300

    Конкатенация

---
+++ src/test/java/datatypes/Lesson27.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson27 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        System.out.println("hexlet" + '-' + 7);
+        // END
+    }
+}
