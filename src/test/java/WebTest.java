3ab8e56224be8e1a5c8eb711c29488e214bdbf43
Vovan <vovan844@gmail.com>
2022-01-20 22:58:37 +0300

    Добавили тест на selenide

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,7 +1,13 @@
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class WebTest {
 
@@ -13,5 +19,14 @@ public class WebTest {
         browser.findElement(By.cssSelector("input#text")).sendKeys("Букмекерская контора марафон");
         browser.findElement(By.cssSelector("button.button_theme_search")).click();
         browser.close();
+    }
+
+    @Test
+    void test2() {
+        Configuration.browser = "opera";
+        Selenide.open("http://ya.ru");
+        $("input#text").sendKeys("Букмекерская контора марафон");
+        $("button.button_theme_search").click();
+        $$("div.VanillaReact.OrganicTitle").shouldBe(CollectionCondition.size(15));
     }
 }
