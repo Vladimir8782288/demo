e8b99168c18c893f5a8d0b5bf8fc9999301ecacc
Vovan <vovan844@gmail.com>
2022-01-21 22:23:10 +0300

    Явное преобразование типов

---
+++ src/test/java/datatypes/Lesson29.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson29 {
+    @Test
+    void test() {
+        // BEGIN (write your solution here)
+        System.out.println((int) 2.9 + " times");
+        // END
+    }
+}
