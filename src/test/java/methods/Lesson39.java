605a2761d840f7f8bf0b0e13c59ecb8a7639cf7c
Vovan <vovan844@gmail.com>
2022-01-23 00:52:44 +0300

    Создание (определение) метода

---
+++ src/test/java/methods/Lesson39.java
@@ -0,0 +1,12 @@
+package methods;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson39 {
+    @Test
+    void testprintMotto() {
+            System.out.println("Winter is coming");
+        }
+        // END
+    }
+
