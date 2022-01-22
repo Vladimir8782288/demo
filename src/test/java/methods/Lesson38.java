ad7660c7b5dcad172294f1189f9de015d04390c1
Vovan <vovan844@gmail.com>
2022-01-23 00:02:23 +0300

    Вызовы статических методов

---
+++ src/test/java/methods/Lesson38.java
@@ -0,0 +1,14 @@
+package methods;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson38 {
+    @Test
+    void test()  {
+        var emoji = "-(";
+        // BEGIN
+        var happyEmoji = ":" + emoji.replace('(', ')');
+        System.out.println(happyEmoji);
+        // END
+    }
+}
