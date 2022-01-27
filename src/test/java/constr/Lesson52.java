1a3b57b67a2d0953fd6126015c9e1628f772cb1f
Vovan <vovan844@gmail.com>
2022-01-28 00:15:36 +0300

    Конструкция Switch

---
+++ src/test/java/constr/Lesson52.java
@@ -0,0 +1,24 @@
+package constr;
+
+import org.junit.jupiter.api.Test;
+
+public class Lesson52 {
+    @Test
+    void test() {
+        System.out.println(getNumberExplanation(666));
+        System.out.println(getNumberExplanation(42));
+        System.out.println(getNumberExplanation(7));
+    }
+    public String getNumberExplanation(int number) {
+        switch (number) {
+            case 666:
+                return "devil number";
+            case 7:
+                return "prime number";
+            case 42:
+                return "answer for everything";
+            default:
+                return null;
+        }
+    }
+}
